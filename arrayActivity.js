0f60183d9a65b03aee093dc06d8c7168792d894f
ronronn <ronaldlegaspi960@gmail.com>
2025-04-05 22:32:59 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ["Singer1", "Singer2", "Singer3"]
+
+console.log(favSingers[0]);
+
+let favNumbers = [1, 2, 3, 4];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
